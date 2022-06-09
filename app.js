522c3f7965d76f979f49afcf4d8430ca51307872
bikokwa <34458781+bikokwa@users.noreply.github.com>
2022-06-09 11:25:49 +0300

    variables - var,let and const

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
-console.log('Hello world');
-console.log(123);
-console.log(true);
-console.table({a:1,b:2})
-console.error('This is some error')
-console.clear()
-console.time('Hello')
-console.log('Hello world')
-console.log('Hello world')
-console.log('Hello world')
-console.log('Hello world')
-console.log('Hello world')
-console.log('Hello world')
-console.timeEnd('Hello')
+// var, let, const
+
+var name = 'John Doe';
+console.log(name)
+name = 'Steve Smith';
+console.log(name)
+
+// Init var
+var greeting;
+console.log(greeting)
+greeting = 'Hello';
+console.log(greeting)
+
+const person = {
+    name: 'John',
+    age: 30
+}
+
+console.log(person)
+
+person.name = 'Sara';
+
+console.log(person)
+
+const numbers = [1,2,3,4,5];
+console.log(numbers)
+
+numbers.push(6)
+console.log(numbers)
