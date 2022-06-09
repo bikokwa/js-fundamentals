ec9aefd9bd21824cb96ee9c8f93ec9653ed27545
bikokwa <34458781+bikokwa@users.noreply.github.com>
2022-06-09 11:04:30 +0300

    using the console

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log('Hello world');
+console.log(123);
+console.log(true);
+console.table({a:1,b:2})
+console.error('This is some error')
+console.clear()
+console.time('Hello')
+console.log('Hello world')
+console.log('Hello world')
+console.log('Hello world')
+console.log('Hello world')
+console.log('Hello world')
+console.log('Hello world')
+console.timeEnd('Hello')
